5eeb5e2d3e099bb406b8686880c86c0f06c7b585
Adriel Vazquez <adrialvaz@hotmail.com>
2023-11-17 15:53:19 -0300

    update para porfolio

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ const parciales = [
         id: 1,
         materia: "Matematica",
         unidades: "(Capítulo 1,2 y 3) de 18 a 22 hs. Cuestionarios para elegir opciones. Puede haber defensa oral",
-        fecha: "11 Sep 2023 18:00:00",
+        fecha: "11 Sep 2024 18:00:00",
         duracion: "1:30HS"
 
     },
@@ -11,7 +11,7 @@ const parciales = [
         id: 2,
         materia: "Programacion",
         unidades: "Práctica y cierre de actividades",
-        fecha: "13 Sep 2023 18:00:00",
+        fecha: "13 Sep 2024 18:00:00",
         duracion: "Duracion desconocida"
 
     },
@@ -19,7 +19,7 @@ const parciales = [
         id: 3,
         materia: "Ingles",
         unidades: "1 evaluacion integradora - Trabajo en foros",
-        fecha: "07 Sep 2023 18:00:00",
+        fecha: "07 Sep 2024 18:00:00",
         duracion: "Fecha de entrega 14/09"
 
     },
@@ -27,7 +27,7 @@ const parciales = [
         id: 4,
         materia: "Laboratorio",
         unidades: "Examen integrador modalidad 1 y Defensa del examen integrador modalidad 2",
-        fecha: "27 Oct 2023 18:00:00",
+        fecha: "27 Oct 2024 18:00:00",
         duracion: "Duracion desconocida"
 
     },
@@ -35,7 +35,7 @@ const parciales = [
         id: 5,
         materia: "Laboratorio",
         unidades: "Defensa de Examen Integrador Modalidad 2",
-        fecha: "03 Nov 2023 18:00:00",
+        fecha: "03 Nov 2024 18:00:00",
         duracion: "Duracion desconocida"
 
     },
@@ -43,7 +43,7 @@ const parciales = [
         id: 6,
         materia: "Laboratorio",
         unidades: "Defensa de Examen Integrador Modalidad 1 y Defensa de Examen Integrador Modalidad 2",
-        fecha: "10 Nov 2023 18:00:00",
+        fecha: "10 Nov 2024 18:00:00",
         duracion: "Duracion desconocida"
 
     },
@@ -51,7 +51,7 @@ const parciales = [
         id: 7,
         materia: "Laboratorio",
         unidades: "Defensa de Examen Integrador Modalidad 2",
-        fecha: "17 Nov 2023 18:00:00",
+        fecha: "17 Nov 2024 18:00:00",
         duracion: "Duracion desconocida"
 
     },
@@ -59,7 +59,7 @@ const parciales = [
         id: 8,
         materia: "Matematica",
         unidades: "SEGUNDO PARCIAL (Capítulo 4,5 y 6) Cuestionarios para elegir opciones - Puede haber defensa oral",
-        fecha: "30 Oct 2023 18:00:00",
+        fecha: "30 Oct 2024 18:00:00",
         duracion: "1:30HS"
 
     },
@@ -67,7 +67,7 @@ const parciales = [
         id: 9,
         materia: "Inglés",
         unidades: "2° EVALUACIÓN INTEGRADORA - Trabajo en foros",
-        fecha: "26 Oct 2023 18:00:00",
+        fecha: "26 Oct 2024 18:00:00",
         duracion: "Fecha de entrega 02/11"
 
     },
@@ -75,7 +75,7 @@ const parciales = [
         id: 10,
         materia: "Programacion",
         unidades: "Recuperación Etapa 1",
-        fecha: "20 Sep 2023 18:00:00",
+        fecha: "20 Sep 2024 18:00:00",
         duracion: "Duracion desconocida"
 
     },
@@ -83,7 +83,7 @@ const parciales = [
         id: 11,
         materia: "SPD",
         unidades: "De unidad 2 a unidad 4",
-        fecha: "19 Sep 2023 18:00:00",
+        fecha: "19 Sep 2024 18:00:00",
         duracion: "Es entre las 8am y las 23:59. Pero no se la duracion"
 
     },
@@ -91,7 +91,7 @@ const parciales = [
         id: 12,
         materia: "SPD",
         unidades: "TRABAJO DE RAID",
-        fecha: "15 Oct 2023 00:00:00",
+        fecha: "15 Oct 2024 00:00:00",
         duracion: "Eran 40 dias de cuando avisó"
 
     },
@@ -99,7 +99,7 @@ const parciales = [
         id: 13,
         materia: "SPD",
         unidades: "SEGUNDO PARCIAL",
-        fecha: "07 Nov 2023 00:00:00",
+        fecha: "07 Nov 2024 00:00:00",
         duracion: "UNIDADES FINALES"
 
     }
@@ -108,7 +108,7 @@ const parciales = [
             id: 11,
             materia: "random",
             unidades: "Recuperación Etapa 1",
-            fecha: "01 Sep 2023 04:20:00",
+            fecha: "01 Sep 2024 04:20:00",
             duracion: "Duracion desconocida"
     
         } */
