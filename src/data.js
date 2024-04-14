ad7fa267f40b2e623f44f88696fba98475fe2c96
Adriel Vazquez <adrialvaz@hotmail.com>
2024-04-14 11:16:49 -0300

    update

--- src/data.js
+++ src/data.js
@@ -1,119 +1,68 @@
 const parciales = [
     {
         id: 1,
-        materia: "Matematica",
-        unidades: "(Capítulo 1,2 y 3) de 18 a 22 hs. Cuestionarios para elegir opciones. Puede haber defensa oral",
-        fecha: "11 Sep 2024 18:00:00",
-        duracion: "1:30HS"
+        materia: "Programación 2",
+        unidades: "POO, Archivos",
+        fecha: "13 May 2024 18:00:00",
+        duracion: "Tal vez una semana."
 
     },
     {
         id: 2,
-        materia: "Programacion",
-        unidades: "Práctica y cierre de actividades",
-        fecha: "13 Sep 2024 18:00:00",
-        duracion: "Duracion desconocida"
+        materia: "Programacion 2",
+        unidades: "Sobrecarga, memoria dinámica, funciones amigas, polimorfismo.",
+        fecha: "10 Jun 2024 18:00:00",
+        duracion: "Tal vez dos semanas"
 
     },
     {
         id: 3,
-        materia: "Ingles",
-        unidades: "1 evaluacion integradora - Trabajo en foros",
-        fecha: "07 Sep 2024 18:00:00",
-        duracion: "Fecha de entrega 14/09"
+        materia: "Programacion 2",
+        unidades: "Existe un TP Integrador. Solo menciona la fecha de entrega.",
+        fecha: "24 Jun 2024 18:00:00",
+        duracion: "Fecha de entrega 24/06"
 
     },
     {
         id: 4,
-        materia: "Laboratorio",
-        unidades: "Examen integrador modalidad 1 y Defensa del examen integrador modalidad 2",
-        fecha: "27 Oct 2024 18:00:00",
-        duracion: "Duracion desconocida"
+        materia: "Laboratorio 2",
+        unidades: "Defensa del exámen integrador.",
+        fecha: "13 Jun 2024 18:00:00",
+        duracion: "Desconocida."
 
     },
     {
         id: 5,
-        materia: "Laboratorio",
-        unidades: "Defensa de Examen Integrador Modalidad 2",
-        fecha: "03 Nov 2024 18:00:00",
-        duracion: "Duracion desconocida"
+        materia: "Laboratorio 2",
+        unidades: "Defensa del exámen integrador.",
+        fecha: "27 Jun 2024 18:00:00",
+        duracion: "Desconocida."
 
     },
     {
         id: 6,
-        materia: "Laboratorio",
-        unidades: "Defensa de Examen Integrador Modalidad 1 y Defensa de Examen Integrador Modalidad 2",
-        fecha: "10 Nov 2024 18:00:00",
-        duracion: "Duracion desconocida"
+        materia: "Laboratorio 2",
+        unidades: "Defensa del exámen integrador.",
+        fecha: "04 Jul 2024 18:00:00",
+        duracion: "Desconocida."
 
     },
     {
-        id: 7,
-        materia: "Laboratorio",
-        unidades: "Defensa de Examen Integrador Modalidad 2",
-        fecha: "17 Nov 2024 18:00:00",
-        duracion: "Duracion desconocida"
+        id: 6,
+        materia: "Laboratorio 2",
+        unidades: "Exámen final.",
+        fecha: "18 Jul 2024 18:00:00",
+        duracion: "Desconocida."
 
     },
     {
-        id: 8,
-        materia: "Matematica",
-        unidades: "SEGUNDO PARCIAL (Capítulo 4,5 y 6) Cuestionarios para elegir opciones - Puede haber defensa oral",
-        fecha: "30 Oct 2024 18:00:00",
-        duracion: "1:30HS"
-
-    },
-    {
-        id: 9,
-        materia: "Inglés",
-        unidades: "2° EVALUACIÓN INTEGRADORA - Trabajo en foros",
-        fecha: "26 Oct 2024 18:00:00",
-        duracion: "Fecha de entrega 02/11"
-
-    },
-    {
-        id: 10,
-        materia: "Programacion",
-        unidades: "Recuperación Etapa 1",
-        fecha: "20 Sep 2024 18:00:00",
-        duracion: "Duracion desconocida"
-
-    },
-    {
-        id: 11,
-        materia: "SPD",
-        unidades: "De unidad 2 a unidad 4",
-        fecha: "19 Sep 2024 18:00:00",
-        duracion: "Es entre las 8am y las 23:59. Pero no se la duracion"
-
-    },
-    {
-        id: 12,
-        materia: "SPD",
-        unidades: "TRABAJO DE RAID",
-        fecha: "15 Oct 2024 00:00:00",
-        duracion: "Eran 40 dias de cuando avisó"
-
-    },
-    {
-        id: 13,
-        materia: "SPD",
-        unidades: "SEGUNDO PARCIAL",
-        fecha: "07 Nov 2024 00:00:00",
-        duracion: "UNIDADES FINALES"
+        id: 6,
+        materia: "Laboratorio 2",
+        unidades: "Exámen final.",
+        fecha: "25 Jul 2024 18:00:00",
+        duracion: "Desconocida."
 
     }
-
-    /*     {
-            id: 11,
-            materia: "random",
-            unidades: "Recuperación Etapa 1",
-            fecha: "01 Sep 2024 04:20:00",
-            duracion: "Duracion desconocida"
-    
-        } */
-
-
 
 ]
 export default parciales;
